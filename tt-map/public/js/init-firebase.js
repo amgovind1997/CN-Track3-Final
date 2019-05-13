a16fdb1d5ec28694db561648b0f1f7143d988a07
amgovind1997 <amgovind1997@gmail.com>
2019-05-13 22:12:13 +0530

    all files

---
+++ tt-map/public/js/init-firebase.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCC6RmC6yNm6vJxC0N9x1Qdc3Vm6hIcV0M",
+    authDomain: "offl-project.firebaseapp.com",
+    databaseURL: "https://offl-project.firebaseio.com",
+    projectId: "offl-project",
+    storageBucket: "offl-project.appspot.com",
+    messagingSenderId: "250402582700"
+  };
+  firebase.initializeApp(config);
